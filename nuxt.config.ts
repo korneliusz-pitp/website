d4d3a9fed3059044d7d30e39f23f48314ecbfd52
MattA-Official <43476212+MattA-Official@users.noreply.github.com>
2025-12-27 21:58:03 -0600

    add D1 database configuration to Nuxt content module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,7 +31,21 @@ export default defineNuxtConfig({
           STUDIO_GOOGLE_MODERATORS:
             "matt@pupsinthepark.uk,rea@pupsinthepark.uk,beck@pupsinthepark.uk",
         },
+        d1_databases: [
+          {
+            binding: "DB",
+            database_name: "website",
+            database_id: "4edea48e-70c0-4c40-83b5-bacfedeb85b8",
+          },
+        ],
       },
+    },
+  },
+
+  content: {
+    database: {
+      type: "d1",
+      bindingName: "DB",
     },
   },
 });
