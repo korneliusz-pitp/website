2dc15c8f3d32657588d2f06b19e8afc502d63ca3
MattA-Official <43476212+MattA-Official@users.noreply.github.com>
2025-12-30 20:30:36 -0600

    nicer error page

---
+++ app/error.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+defineProps<{
+  error: NuxtError
+}>()
+</script>
+
+<template>
+  <UApp>
+    <UHeader />
+
+    <UError :error="error" />
+
+    <UFooter />
+  </UApp>
+</template>
