957ce5110c22dd4e8e5e3d9480ed725d4e369838
Matt Adcock <43476212+MattA-Official@users.noreply.github.com>
2026-01-20 00:55:57 +0000

    Add 'discord' color (updated on homepage - elsewhere needs changing)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/eslint", "@nuxt/image", "@nuxt/ui", "@nuxt/content", "nuxt-studio", "@nuxt/a11y"],
+  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui', '@nuxt/content', 'nuxt-studio', '@nuxt/a11y'],
+
+  $production: {
+    image: {
+      provider: 'cloudflare',
+      cloudflare: {
+        baseURL: 'https://preview.pupsinthepark.uk/', // TODO: Update this when needed lol
+      },
+    },
+  },
 
   devtools: { enabled: true },
 
-  compatibilityDate: "2025-07-15",
+  css: ['~/assets/css/main.css'],
 
-  css: ["~/assets/css/main.css"],
+  content: {
+    database: {
+      type: 'd1',
+      bindingName: 'DB',
+    },
+  },
 
   ui: {
     theme: {
       colors: [
         // Semantic Roles (Required)
-        "primary",
-        "secondary",
-        "neutral",
-        "error",
-        "warning",
-        "success",
-        "info",
+        'primary',
+        'secondary',
+        'neutral',
+        'error',
+        'warning',
+        'success',
+        'info',
+        'discord',
 
         // Brand Role Scales (TODO: Add these)
         // "helper-red",
@@ -31,55 +46,41 @@ export default defineNuxtConfig({
     },
   },
 
-  studio: {
-    repository: {
-      provider: "github",
-      owner: "pups-in-the-park",
-      repo: "website",
-      branch: "main",
-    },
-  },
+  compatibilityDate: '2025-07-15',
 
   nitro: {
-    preset: "cloudflare_module",
+    preset: 'cloudflare_module',
     cloudflare: {
       deployConfig: true,
       nodeCompat: true,
       wrangler: {
         vars: {
           STUDIO_GOOGLE_MODERATORS:
-            "matt@pupsinthepark.uk,rea@pupsinthepark.uk,beck@pupsinthepark.uk",
+            'matt@pupsinthepark.uk,rea@pupsinthepark.uk,beck@pupsinthepark.uk',
         },
         d1_databases: [
           {
-            binding: "DB",
-            database_name: "website",
-            database_id: "ab3472a7-a1c9-40a6-a240-feb4f241c9e3",
+            binding: 'DB',
+            database_name: 'website',
+            database_id: 'ab3472a7-a1c9-40a6-a240-feb4f241c9e3',
           },
         ],
       },
     },
   },
 
-  content: {
-    database: {
-      type: "d1",
-      bindingName: "DB",
-    },
-  },
-
-  $production: {
-    image: {
-      provider: "cloudflare",
-      cloudflare: {
-        baseURL: "https://preview.pupsinthepark.uk/", // TODO: Update this when needed lol
-      }
-    },
-  },
-
   eslint: {
     config: {
       stylistic: true,
-    }
-  }
-});
+    },
+  },
+
+  studio: {
+    repository: {
+      provider: 'github',
+      owner: 'pups-in-the-park',
+      repo: 'website',
+      branch: 'main',
+    },
+  },
+})
