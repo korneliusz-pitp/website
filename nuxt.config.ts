2878143bdbc9e8e0b91dacc89ec20fa11e40d93e
Matt Adcock <43476212+MattA-Official@users.noreply.github.com>
2026-01-26 16:56:25 +0000

    configure SEO and schema.org

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,16 @@
+import { defineOrganization } from 'nuxt-schema-org/schema'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui', '@nuxt/content', 'nuxt-studio', '@nuxt/a11y'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/ui',
+    '@nuxt/content',
+    'nuxt-studio',
+    '@nuxt/a11y',
+    '@nuxtjs/seo',
+  ],
 
   $production: {
     image: {
@@ -14,6 +24,13 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
 
   css: ['~/assets/css/main.css'],
+
+  site: {
+    url: 'https://pupsinthepark.uk',
+    name: 'Pups in the Park',
+    description: 'A UK kemonomimi and alternative community meet organisation bringing together pups, handlers, and friends for safe, inclusive events.',
+    defaultLocale: 'en-GB',
+  },
 
   content: {
     database: {
@@ -76,6 +93,69 @@ export default defineNuxtConfig({
     config: {
       stylistic: true,
     },
+  },
+
+  robots: {
+    groups: [
+      {
+        userAgent: '*',
+        disallow: ['/api/', '/_nuxt/'],
+      },
+    ],
+  },
+
+  schemaOrg: {
+    identity: defineOrganization({
+      name: 'Pups in the Park',
+      description: 'The largest UK kemonomimi and alternative community meet. Bringing together therians, furries, kemonomimis, and alterhumans for safe, inclusive events.',
+      url: 'https://pupsinthepark.uk',
+      logo: '/images/brand/logo.png',
+      email: 'info@pupsinthepark.uk',
+      contactPoint: [
+        {
+          '@type': 'ContactPoint',
+          'email': 'info@pupsinthepark.uk',
+          'contactType': 'General Enquiries',
+        },
+        {
+          '@type': 'ContactPoint',
+          'email': 'events@pupsinthepark.uk',
+          'contactType': 'Events Team',
+        },
+        {
+          '@type': 'ContactPoint',
+          'email': 'safeguarding@pupsinthepark.uk',
+          'contactType': 'Safeguarding',
+        },
+        {
+          '@type': 'ContactPoint',
+          'email': 'volunteer@pupsinthepark.uk',
+          'contactType': 'Volunteer Enquiries',
+        },
+      ],
+      sameAs: [
+        'https://www.instagram.com/pupsintheparkuk/',
+        'https://www.tiktok.com/@pupsintheparkuk',
+        'https://discord.gg/9Sz2msbSDq',
+        'https://www.gofundme.com/f/pupsinthepark',
+      ],
+      knowsAbout: [
+        'https://pupsinthepark.uk/policies/code-of-conduct',
+        'https://pupsinthepark.uk/policies/privacy',
+        'https://pupsinthepark.uk/policies/safeguarding-adults',
+        'https://pupsinthepark.uk/policies/safeguarding-children',
+        'https://pupsinthepark.uk/policies/safety',
+        'https://pupsinthepark.uk/policies/media-photography',
+        'https://pupsinthepark.uk/policies/equality-diversity-inclusion',
+        'https://pupsinthepark.uk/policies/environmental-impact',
+        'https://pupsinthepark.uk/policies/online-safety',
+        'https://pupsinthepark.uk/policies/complaints-grievances',
+      ],
+    }),
+  },
+
+  sitemap: {
+    sources: ['/api/__sitemap__/urls'],
   },
 
   studio: {
